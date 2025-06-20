a78c55d7e111a9ce2f27548beac29007ac4f05b9
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2025-06-20 16:49:03 +0700

    Init Create

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
