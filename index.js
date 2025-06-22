564e7519ce98e8566c6cf072a4035f46d13e13b5
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2025-06-22 16:58:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/metrics', async (req, res) => {
 });
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+  res.send({ message: 'Hello from backend!' });
 });
 
 app.listen(port, () => {
