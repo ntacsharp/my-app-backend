3135be48fcfabfcc6f61519edae8c9b0b207ca38
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2025-06-22 16:50:06 +0700

    Prom

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const client = require('prom-client');
 const cors = require('cors');
 const app = express();
 const port = 5000;
 
 app.use(cors());
 
-app.get('/api/hello', (res) => {
+client.collectDefaultMetrics();
+
+const requestCounter = new client.Counter({
+  name: 'backend_requests_total',
+  help: 'Total HTTP requests to backend',
+  labelNames: ['path', 'method'],
+});
+
+app.use((req, res, next) => {
+  requestCounter.labels(req.path, req.method).inc();
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
